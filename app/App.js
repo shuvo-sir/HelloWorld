7fd9feb5030f908901817fda55f1e2bdad543d45
Shuvo Halder <shuvohalder499@gmail.com>
2025-06-10 23:46:21 +0600

    box design

--- app/App.js
+++ app/App.js
@@ -1,15 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet} from "react-native";
 
 export default function App() {
     return(
         <View style={styles.container}>
-            <Text style = {styles.title}>StyleSheet API</Text>
+           <View style={[styles.box, styles.lightblueBg]}> 
+                 <Text >Lightblue</Text>
+           </View>
+           <View style={[styles.box, styles.lightgreenBg]}>
+                 <Text >Lightgreen</Text>
+           </View>
         </View>
+
     );
 }
 
 
 const styles = StyleSheet.create({
     container: {flex: 1, backgroundColor: "plum", padding: 60},
-    title: {},
+    box: {
+        width: 100,
+        height: 100,
+        paddingHorizontal: 10,
+        paddingVertical: 20,
+        marginVertical: 10,
+        borderWidth: 2,
+        borderColor: "purple",
+        borderRadius: 5,
+    },    
+    lightblueBg: {
+        backgroundColor: "lightblue",
+    },
+    lightgreenBg: {
+        backgroundColor: "lightgreen",
+    }
 });
+

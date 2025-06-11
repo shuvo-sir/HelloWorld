b2f165ac2735f07406d8bf621bca7c809fa83bc5
Shuvo Halder <shuvohalder499@gmail.com>
2025-06-11 21:53:28 +0600

    Shado and style inharitance

--- app/App.js
+++ app/App.js
@@ -3,10 +3,13 @@ import { View, Text, StyleSheet} from "react-native";
 export default function App() {
     return(
         <View style={styles.container}>
+            <View style={[styles.darkMode]}>
+                <Text style= {styles.textColor}>Style Inheritance</Text>
+            </View>
            <View style={[styles.box, styles.lightblueBg]}> 
                  <Text >Lightblue</Text>
            </View>
-           <View style={[styles.box, styles.lightgreenBg]}>
+           <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
                  <Text >Lightgreen</Text>
            </View>
         </View>
@@ -17,9 +20,16 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {flex: 1, backgroundColor: "plum", padding: 60},
+    darkMode: {
+        backgroundColor: "black",
+    },
+    textColor: {
+        color: "white",
+        textAlign: "center",
+    },
     box: {
-        width: 100,
-        height: 100,
+        width: 250,
+        height: 250,
         paddingHorizontal: 10,
         paddingVertical: 20,
         marginVertical: 10,
@@ -32,6 +42,9 @@ const styles = StyleSheet.create({
     },
     lightgreenBg: {
         backgroundColor: "lightgreen",
+    },
+    androidShadow: {
+        elevation: 10,
     }
 });
 

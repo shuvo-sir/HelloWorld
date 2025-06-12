09a5d7542c8cbd41addd72d4685345d15f8bcb66
Shuvo Halder <shuvohalder499@gmail.com>
2025-06-12 11:11:11 +0600

    justifycontent

--- app/App.js
+++ app/App.js
@@ -1,50 +1,27 @@
-import { View, Text, StyleSheet} from "react-native";
+import { StyleSheet, View } from "react-native";
+import Box from "../constants/Box";
 
 export default function App() {
     return(
         <View style={styles.container}>
-            <View style={[styles.darkMode]}>
-                <Text style= {styles.textColor}>Style Inheritance</Text>
-            </View>
-           <View style={[styles.box, styles.lightblueBg]}> 
-                 <Text >Lightblue</Text>
-           </View>
-           <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
-                 <Text >Lightgreen</Text>
-           </View>
+            <Box style={{backgroundColor: "#8e9b00"}}>Box 1</Box>
+            <Box style={{backgroundColor: "#f39c12"}}>Box 2</Box>
+            <Box style={{backgroundColor: "#e74c3c"}}>Box 3</Box>
+            <Box style={{backgroundColor: "#2ecc71"}}>Box 4</Box>
+            <Box style={{backgroundColor: "#3498db"}}>Box 5</Box>
+            <Box style={{backgroundColor: "#9b59b6"}}>Box 6</Box>
+            <Box style={{backgroundColor: "#1abc9c"}}>Box 7</Box>
+            <Box style={{backgroundColor: "#e67e22"}}>Box 8</Box>
+            <Box style={{backgroundColor: "#34495e"}}>Box 9</Box>
         </View>
-
     );
 }
 
-
 const styles = StyleSheet.create({
-    container: {flex: 1, backgroundColor: "plum", padding: 60},
-    darkMode: {
-        backgroundColor: "black",
+    container: {
+        flex: 1,
+        borderWidth: 6,
+        borderColor: "red",
+        justifyContent: "space-evenly"
     },
-    textColor: {
-        color: "white",
-        textAlign: "center",
-    },
-    box: {
-        width: 250,
-        height: 250,
-        paddingHorizontal: 10,
-        paddingVertical: 20,
-        marginVertical: 10,
-        borderWidth: 2,
-        borderColor: "purple",
-        borderRadius: 5,
-    },    
-    lightblueBg: {
-        backgroundColor: "lightblue",
-    },
-    lightgreenBg: {
-        backgroundColor: "lightgreen",
-    },
-    androidShadow: {
-        elevation: 10,
-    }
 });
-
